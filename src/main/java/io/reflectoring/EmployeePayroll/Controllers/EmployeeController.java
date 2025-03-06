cf17e36ecd91dc6fe17f53954a919d22a9562c42
prakhar8400 <prakharm495@gmail.com>
2025-03-06 12:40:21 +0530

    Delete Files

--- src/main/java/io/reflectoring/EmployeePayroll/Controllers/EmployeeController.java
+++ src/main/java/io/reflectoring/EmployeePayroll/Controllers/EmployeeController.java
@@ -16,30 +16,6 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @PostMapping("/post")
-    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        return (employeeService.createEmployee(employeeDTO));
-    }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<EmployeeDTO> getEmployeeById(@PathVariable Long id) {
-        return ResponseEntity.ok(employeeService.getEmployeeById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<EmployeeDTO>> getAllEmployees() {
-        return ResponseEntity.ok(employeeService.getAllEmployees());
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDTO employeeDTO) {
-        return ResponseEntity.ok(employeeService.updateEmployee(id, employeeDTO));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
-        employeeService.deleteEmployee(id);
-        return ResponseEntity.ok("Employee deleted successfully");
-    }
 }
 
